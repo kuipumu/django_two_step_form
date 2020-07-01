b2a0616089d6ff275d87a183893448a56947af60
kuipumu <kuipumu@cryptolab.net>
2020-07-01 14:53:12 -0400

    prepare for heroku deploy.

--- app/settings.py
+++ app/settings.py
@@ -105,9 +105,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': env.db()
-}
+if !HEROKU:
+    DATABASES = {
+        'default': env.db()
+    }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
